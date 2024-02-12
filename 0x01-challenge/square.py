9ae83a195f7e9f1a8d246f3eb96742b71402d911
MoWael45 <mowael450@gmail.com>
2024-02-13 00:24:37 +0200

    update

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
 
-# Editting 1: Make the class Square not square
 class Square():
-    # Editing 2: Adding Doc
-    """ start the class"""
-
-    # Edit 3: We don't need kwargs, we just will use width and height
-    def __init__(self, width, height):
-    # Edit 4: Adding doc
-        """ init the class"""
-        self.width = width
-        self.height = height
+    
+    def __init__(self, side_length=0):
+        self.side_length = side_length
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.side_length * self.side_length
 
-    def PermiterOfMySquare(self):
-        # Edit 5: adding doc
-        """ permmiter of the square"""
-        # Edit 6: Exchange the permiter of rectangle by the square
-        return 4 * self.width
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
+        return 4 * self.side_length
 
     def __str__(self):
-        # Edit 7: Adding doc
-        """ String representation """
-        return "{}/{}".format(self.width, self.height)
+        return "Square with side length: {}".format(self.side_length)
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = Square(side_length=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
