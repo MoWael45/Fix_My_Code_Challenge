287e60f50218c71499272e92e57bd0015dce350b
MoWael45 <mowael450@gmail.com>
2024-02-13 00:21:27 +0200

    edit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,48 +1,35 @@
 #!/usr/bin/python3
-""" A python file which contains a class with the
-properties and operations of a square"""
 
-class Square:
-    """
-    A class representing a square.
-    
-    Attributes:
-    - width: The width of the square.
-    - height: The height of the square.
-    """
+# Editting 1: Make the class Square not square
+class Square():
+    # Editing 2: Adding Doc
+    """ start the class"""
 
-    def __init__(self, width=0, height=0):
-        """
-        Initializes a square with the given width and height.
-        
-        Args:
-        - width: The width of the square.
-        - height: The height of the square.
-        """
+    # Edit 3: We don't need kwargs, we just will use width and height
+    def __init__(self, width, height):
+    # Edit 4: Adding doc
+        """ init the class"""
         self.width = width
         self.height = height
 
     def area_of_my_square(self):
-        """
-        Returns the area of the square.
-        """
-        return self.width * self.height
+        """ Area of the square """
+        return self.width * self.width
 
-    def perimeter_of_my_square(self):
-        """
-        Returns the perimeter of the square.
-        """
-        return (self.width * 2) + (self.height * 2)
+    def PermiterOfMySquare(self):
+        # Edit 5: adding doc
+        """ permmiter of the square"""
+        # Edit 6: Exchange the permiter of rectangle by the square
+        return 4 * self.width
 
     def __str__(self):
-        """
-        Returns a string representation of the square
-        in this format 'width/height'.
-        """
+        # Edit 7: Adding doc
+        """ String representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
+
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.PermiterOfMySquare())
